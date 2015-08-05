1acf4830e2883e0e5ef3341c456ccf20d69795d4
Artem Bursuk <a.bursuk@gmail.com>
2015-08-05 08:49:08 +0300

    Alpha

--- Server/controller.py
+++ Server/controller.py
@@ -12,10 +12,24 @@ class InstallProc(object):
 									self.json_source['servers']['server_{0}'.format(i)]['patch_{num}'.format(num = j)]['order_num'], \
 									self.json_source['servers']['server_{0}'.format(i)]['patch_{num}'.format(num = j)]['patch']))
 			self.__dict__['server_{0}'.format(i)] = patchs_list
+			if not os.path.exists('servers/{folder}'.format(self.__dict__['server_{0}'.format(i)][1])):
+    			os.makedirs('servers/{folder}'.format(self.__dict__['server_{0}'.format(i)][1]))
+    		if not os.path.exists('servers/{folder}/{id}'.format(self.__dict__['server_{0}'.format(i)][1], self.__dict__['server_{0}'.format(i)][0])):
+    			with open('servers/{folder}/{id}.lock'.format(self.__dict__['server_{0}'.format(i)][1], self.__dict__['server_{0}'.format(i)][0]), 'w') as lock_file:
+    				lock_file.write('start')
+    			lock_file.close()
+		# created lock file(empty). mask is [order_number],[status]
 
-		# 1. create folders for each server_id
-		# 2. create lock file(empty). mask is [order_number],[status]
-
-	def get_status(self, server_id):
-		pass
+	def get_status(self, server_id, patch_num):
+		if not os.path.exists('servers/{server_id}.lock'.format(server_id = server_id)):
+			return True
+		else:
+			with open('servers/{server_id}.lock'.format(server_id = server_id), 'r') as lock_file:
+   				arr = lock_file.readlines()
+			lock_file.close()
+			server_info = tuple(str([item for item in arr if item != '\n'][-1]).strip(' \t\n\r').split(', '))
+			if server_info[1] == 'PASSED' and int(server_info[0]) == patch_num:
+				return True
+			else:
+				return False
 		# parse file and return last row as ([order_num], [status]) or None in case file is empty
--- Server/main.py
+++ Server/main.py
@@ -5,7 +5,16 @@ from twisted.internet import reactor
 from controller import InstallProc
 import json, sys
 
-port = int(sys.argv[1])
+port = 8007
+
+class MyPP(protocol.ProcessProtocol):
+	def connectionMade(self):
+		self.pid = self.transport.pid
+		print "connectionMade {pid}".format(pid = self.pid)
+
+	def processExited(self, reason):
+		print "processExited {pid}, status {status}".format(pid = self.pid, status = reason)
+
 
 class BuildConfig(Protocol):
 	message = ''
@@ -18,15 +27,21 @@ class BuildConfig(Protocol):
 		if '"end": 1}' in self.message:
 			request = json.loads(self.message)
 			server_installation = InstallProc(request)
-			# for i in xrange(server_installation.servers_num):
-			# test ProcessProtocol
-			# 1. make loop to start processes 
-			# if ... pasre lockfile..
-			# or just use processExited to start another one in case there are more patches..
-	def connectionLost(self):
-		print 1
+			self.run_processes(server_installation)
 
-		
+	def run_processes(self, server_installation_obj):
+		for i in xrange(server_installation_obj.servers_num):
+			for j in xrange(len(server_installation_obj['servers']['server_{0}'.format(i)])):
+				command.extend(to_list(server_installation_obj['servers']['server_{0}'.format(i)][j]))
+				# <TBD>
+				server_id = server_installation_obj['server_{0}'.format(i)][j][0]
+				patch_num = server_installation_obj['server_{0}'.format(i)][j][1]-1
+				if server_installation_obj.get_status(server_id, patch_num):
+					pp = MyPP()
+					command = ['C:\Python27\python.exe','installer.py']
+					subprocess = reactor.spawnProcess(pp, command[0], command, env=os.environ)
+					print "Process for {pid} started..".format(pid = pp.pid)
+					
 
 class BCFactory(Factory):
 	"""docstring for BCFactory"""
@@ -34,12 +49,10 @@ class BCFactory(Factory):
 	def buildProtocol(self, addr):
 		return BuildConfig(self)
 
-
 def main():
 	endpoint = TCP4ServerEndpoint(reactor, port)
 	endpoint.listen(BCFactory())
 	reactor.run()
-
 
 # https://twistedmatrix.com/documents/8.1.0/api/twisted.internet.protocol.ProcessProtocol.html
 if __name__ == '__main__':
