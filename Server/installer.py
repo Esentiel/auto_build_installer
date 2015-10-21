98b136f5338a32a5840e002fb11424c827c9ae32
unknown <bursuk@wsua-0847.netcracker.com>
2015-10-21 17:47:03 +0300

    added loading variables into paramiko

--- Server/installer.py
+++ Server/installer.py
@@ -83,9 +83,9 @@ sftp.put('servers/{instnc}/dbschememanager.xml'.format(instnc = instance_name), 
 	'/netcracker/config/{inctnc}/AutoInstaller/dbschememanager.xml'.format(inctnc = instance_name))
 
 if 'data-toolkit-restore' in patch_name:
-	command = 'cd /netcracker/config/{instnc}; unzip -oq {pch}; ./install.sh dtk.schema_to_restore={domain} > /dev/null 2>&1 &'.format(instnc = instance_name,pch = patch_name, domain = domain_id)
+	command = 'source ~/.bash_profile && cd /netcracker/config/{instnc}; unzip -oq {pch}; ./install.sh dtk.schema_to_restore={domain} > /dev/null 2>&1 &'.format(instnc = instance_name,pch = patch_name, domain = domain_id)
 else:
-	command = 'cd /netcracker/config/{instnc}; unzip -oq {pch}; ./install.sh > /dev/null 2>&1 &'.format(instnc = instance_name,pch = patch_name)
+	command = 'source ~/.bash_profile && cd /netcracker/config/{instnc}; unzip -oq {pch}; ./install.sh > /dev/null 2>&1 &'.format(instnc = instance_name,pch = patch_name)
 client.exec_command(command)
 time.sleep(20)
 command = 'tail -65 /netcracker/config/{instnc}/installer_logs/installer.log'.format(instnc = instance_name)
